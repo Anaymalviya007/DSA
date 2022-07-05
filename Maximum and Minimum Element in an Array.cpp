6929f0b3c9d796891984f58578cf822727a11403
DECODE <58022323+Anaymalviya007@users.noreply.github.com>
2022-07-05 23:21:29 +0700

    Create 	Maximum and Minimum Element in an Array.cpp

---
+++ Maximum and Minimum Element in an Array.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+    
+    long max,min;
+    
+    if( a[0] < a[1] ) {
+        min = a[0];
+        max = a[1];
+    }
+    else{
+        min = a[1];
+        max = a[0];
+    }
+    
+    for (int i = 2; i<n; i++) {
+        if(a[i] > max) {
+            max = a[i];
+        }
+        
+        if(a[i] < min) {
+            min = a[i];
+        }
+    
+    }
+        
+        return {min,max};
+    
+}A
